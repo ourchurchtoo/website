28d824dc0173b71ed33fa2620dd42a3bcbaf4ab6
Benjamin Hollway <benjamin@nothingrandom.com>
2020-08-13 17:58:32 +0200

    Add mailchimp form signup

---
+++ src/js/modules/mailchimp.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('newsletter');
+
+const submitEmail = (email) => {
+  fetch('/.netlify/functions/form-handler', {
+    method: 'post',
+    body: JSON.stringify({
+      email,
+    }),
+  }).then((response) => response.json()).then((data) => {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerText = 'Confirmation email has been sent!';
+  });
+};
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('inputEmail').value;
+  submitEmail(email);
+});
